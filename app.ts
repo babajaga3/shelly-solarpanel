e6de2c904306aa7b6423bbf4ca26859ba6f87198
Toma Bourov <t.bourov@gmail.com>
2024-08-12 12:56:40 +0300

    :construction: Add initial job scheduler

--- app.ts
+++ app.ts
@@ -1,6 +1,23 @@
-const today = new Date().toISOString().split('T')[0];
+import Bree from 'bree';
+import path from 'path';
+import {fileURLToPath} from "node:url";
+import breeTsWorker from '@breejs/ts-worker'
 
-console.log('Today: ', today);
-console.log('Now: ', now);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+Bree.extend(breeTsWorker);
+const bree = new Bree({
+    logger: console,
+    acceptedExtensions: ['ts'],
+    jobs: [
+        {
+            name: 'shelly',
+            path: path.join(__dirname, 'jobs', 'shelly.ts'),
+            cron: '5 * * * *' // Run every hour at 5 minutes past the hour (e.g. 01:05, 02:05, etc.)
+        }
+    ]
+})
 
+// Start the jobs
+console.log('Starting jobs...');
+await bree.start();
