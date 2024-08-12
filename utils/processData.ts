45d6a6d4a07e3fca3d2b390a1d1cbe6ea9283767
Toma Bourov <t.bourov@gmail.com>
2024-08-12 12:55:08 +0300

    :hammer: Improve processData workflow

--- utils/processData.ts
+++ utils/processData.ts
@@ -6,35 +6,34 @@ import { CsvRecord } from "../types";
 
 export async function processData() {
   const today = new Date().toISOString().split("T")[0];
+  const filePath = `./downloads/DAM Result for_ ${today}.csv`;
   let commandArray: number[] = [];
 
-  // Download CSV file with data if it does not exist
-  if (!fs.existsSync(`./downloads/DAM Result for_ ${today}.csv`)) {
-    console.log('Could not find file, downloading...');
-    await downloadCsv();
-    console.log('Download complete');
-  }
+  try {
+    // Download CSV file with data if it does not exist
+    if (!fs.existsSync(filePath)) {
+      console.log('Could not find file, downloading...');
+      await downloadCsv();
+      console.log('Download complete');
+    }
 
-  /**
-   * Convert CSV file to JSON
-   * For every entry in the data array,
-   * push the command number to the commandArray based on the 'Price (BGN)'
-   * 0 - Off; 1 - On
-   */
-  console.log('Converting CSV to JSON...');
-  await convertCsv(`./downloads/DAM Result for_ ${today}.csv`).then((data: CsvRecord[]) => {
-    data.forEach((entry) => {
-      if (parseFloat(entry["Price (BGN)"]) < 7) {
-        commandArray.push(0);
-      } else {
-        commandArray.push(1);
-      }
-    });
+    // Convert CSV file to JSON
+    console.log('Converting CSV to JSON...');
+    const data: CsvRecord[] = await convertCsv(filePath);
+
+    // Process each entry in the data array
+    commandArray = data.map(entry =>
+        parseFloat(entry["Price (BGN)"]) < 7 ? 0 : 1
+    );
+
     console.log('Conversion complete');
-  });
 
-  // Save data as JSON file
-  console.log('Saving data as JSON...');
-  await saveJson(commandArray);
-  console.log('Data saved as JSON');
+    // Save data as JSON file
+    console.log('Saving data as JSON...');
+    await saveJson(commandArray);
+    console.log('Data saved as JSON');
+
+  } catch (error) {
+    console.error('An error occurred during the process:', error);
+  }
 }
